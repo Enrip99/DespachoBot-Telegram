6be57cd090502a53bdaaa3dfd56b2b0c04283b1f
VGAFIB-Inky <vgafib@gmail.com>
2022-09-08 12:09:30 +0200

    Support for webcam

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const config = require('./data/config.json');
+const NodeWebcam = require("node-webcam");
+
+const opts = {
+	width: 640,
+	height: 480,
+	quality: 50,
+	frames: 1,
+	delay: 0,
+	saveShots: true,
+	output: "png",
+	device: false,
+	callbackReturn: "location",
+	verbose: false
+};
+const Webcam = NodeWebcam.create( opts );
 
 const bot = new TelegramBot(config.token, {polling: true});
 
 bot.onText(/^(alguien despacho(\?*)|\/alguiendespacho)$/i, (msg, match) => {
 		takePicAndSend(msg.chat.id)
-		console.log(msg)
 })
 
 bot.on('sticker', (msg) => {
@@ -17,6 +31,13 @@ bot.on('sticker', (msg) => {
 
 function takePicAndSend(chatId){
 	if (chatId == config.ChatId){
-		bot.sendPhoto(chatId, 'data/Foto.png')
+		Webcam.capture( "data/Foto", function( err, data ) {
+			if (err){
+				bot.sendMessage(chatId, 'Error');
+			}
+			else {
+				bot.sendPhoto(chatId, 'data/Foto.png').catch(e => {console.error(e)});
+			}
+		} );
 	}
 }
